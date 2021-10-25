a1fe4fabb85d700c13b2c964c6ed7f919a90bb37
Rodrigo Rech <rodrigorech92@gmail.com>
2021-10-25 18:02:09 -0300

    Front end

---
+++ src/pages/Card.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+
+import { Card, Button } from 'react-bootstrap';
+
+const { io } =require("socket.io-client");
+const socket = io('http//localhost:3001');
+
+export default function CardItem( id, name, valor, image) {
+  const [currentValue, setCurrentValue] = useState(valor)
+
+  useEffect(() => {
+    socket.on('refreshCurrentValue', (data) => {
+      if(id === data._id) setCurrentValue(valor)
+    })
+  })
+
+  const handleClick = (e) => {
+    socket.emit('increaseValue', { id })
+  }
+
+  return (
+    <Card>
+      <Card.img variant="top" src={image} />
+      <Card.Body>
+        <Card.Title><span>{name}</span></Card.Title>
+        
+        <Card.Text>
+          Valor: <span>{currentValue}</span>  
+        </Card.Text>
+        { currentValue < 100 ?
+          <Button onClick={handleClick}>Votar</Button>
+          :
+          <Button disable={true}>Produto arrematado</Button>
+        }
+        
+      </Card.Body>
+    </Card>
+  )
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react'
+import { CardGroup } from 'react-bootstrap';
+import CardItem from './Card'
+
+const images = ['https://m.media-amazon.com/images/I/91bwGpRgzkL._AC_SL1500_.jpg',
+'https://images.kabum.com.br/produtos/fotos/110161/headset-gamer-razer-kraken-ultimate-chroma-usb-drivers-50mm-rz04-03180100-r3u1_1602677445_g.jpg',
+'https://assets2.razerzone.com/images/og-image/razer-blackwidow-v3-mini-hyperspeed-OGimage.jpg']
+
+export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState([]);
+
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch('http://localhost:3001/items')
+      .then(response => response.json())
+      .then(data => {
+        setItems(data);
+        setIsLoading(false)
+      })
+  });
+
+  return (
+    <div>
+      { isLoading ? <p>Carregando</p>
+      : 
+      <CardGroup>
+        {items.map((item, index) => (
+          <CardItem
+          key={item._id}
+          id={item._id}
+          name={item.name}
+          valor={item.valor}
+          image={images[index]}
+          />
+        ))
+      }
+      </CardGroup>
+      }
+
+    </div>
+  )
+}
+
+
